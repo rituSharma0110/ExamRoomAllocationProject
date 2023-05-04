4b96223e45e904303eeff20a14ae9abdce991ccf
Sanchita Saxena <sanchita.saxena@keysight.com>
2023-05-05 01:34:23 +0530

    style UI

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -28,8 +28,8 @@ function onUpload() {
     xmlhttp1.onreadystatechange = function () {
         if (this.status == 200) {
             var res = this.responseText;
-//            alert("File Uploaded Successfully!!");
-   //         window.location.reload();
+            alert("Files Uploaded Successfully!!");
+            window.location.reload();
         }
     };
 
